3c5176421233bd05662171c92fa6bada2d2b8b27
”fabigo737” <fabiandegouveia@gmail.com>
2022-09-22 20:58:44 -0500

    added pi data privacy security pages and jquery

---
+++ js/jquerycallpages.js
@@ -0,0 +1,8 @@
+route();
+
+function route() {
+    $("#personalinfo").load("Profile/personalinfo.html")
+    $("#dataprivacy").load("Profile/dataprivacy.html")
+    $("#seguridad").load("Profile/security.html")
+    $("#paymenthistory").load("Profile/paymenthistory.html")
+  }  
