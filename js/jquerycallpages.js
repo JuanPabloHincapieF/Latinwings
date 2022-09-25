c6bad46a774d4d4ae82794c36f475eedb5eab388
JuanPabloHincapieF <jphf0527@gmail.com>
2022-09-25 14:43:21 -0500

    Arreglando bugs en vuelos.html

--- js/jquerycallpages.js
+++ js/jquerycallpages.js
@@ -1,9 +1,9 @@
 route();
 
 function route() {
-    $("#personalinfo").load("Profile/personalinfo.html")
-    $("#dataprivacy").load("Profile/dataprivacy.html")
-    $("#seguridad").load("Profile/security.html")
-    $("#paymenthistory").load("Profile/paymenthistory.html")
-    $("#membershipsetup").load("Profile/membership.html")
-  }  
+  $("#personalinfo").load("Profile/personalinfo.html");
+  $("#dataprivacy").load("Profile/dataprivacy.html");
+  $("#seguridad").load("Profile/security.html");
+  $("#paymenthistory").load("Profile/paymenthistory.html");
+  $("#membershipsetup").load("Profile/membership.html");
+}
