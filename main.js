7cca835af160710861760358c9f3795a24e31c39
”fabigo737” <fabiandegouveia@gmail.com>
2022-09-27 21:02:41 -0500

    added login exitoso alert

--- main.js
+++ main.js
@@ -15,6 +15,26 @@ validateLogin();
 
 function entrarcuenta() {
     localStorage.setItem("userName", "wildys");
+    setTimeout(mensaje, 2800)
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2800,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Inicio de sesion exitoso!'
+    })
+  }
+
+  function mensaje(){
     location.reload()
   }
 
